62e2da3d92edc33ad47701b376476323571a6651
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2024-12-16 10:08:03 +0400

    feat: add ability to filter dataset by len

--- dataset.py
+++ dataset.py
@@ -40,13 +40,34 @@ def get_reuters_raw(num_doc=100):
     return corpus
 
 
-def load_dataset(dataset="scifact", split="test"):
+def load_dataset(dataset: str = None, split: str = "test", length: int = None):
+    if not dataset:
+        dataset = "scifact"
+    if length:
+        assert length >= 1
     url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
     out_dir = os.path.join(os.getcwd(), "datasets")
     data_path = util.download_and_unzip(url, out_dir)
     print("Dataset downloaded here: {}".format(data_path))
     corpus, queries, qrels = GenericDataLoader(data_path).load(split=split)  # or split = "train" or "dev"
-    return corpus, queries, qrels
+    if not length:
+        return corpus, queries, qrels
+    filtered_corpus = {doc_id: doc for i, (doc_id, doc) in enumerate(corpus.items()) if i < length}
+    filtered_qrels = {
+        query_id: {
+            doc_id: rel
+            for doc_id, rel in doc_id_to_rel.items()
+            if doc_id in filtered_corpus.keys()
+        }
+        for query_id, doc_id_to_rel in qrels.items()
+        if any(doc_id in filtered_corpus.keys() for doc_id in doc_id_to_rel.keys())
+    }
+    filtered_queries = {query_id: queries[query_id]
+                        for query_id in filtered_qrels}
+    del corpus
+    del queries
+    del qrels
+    return filtered_corpus, filtered_queries, filtered_qrels
 
 
 def get_corpus():
@@ -55,13 +76,10 @@ def get_corpus():
         corpus_df = pd.DataFrame(corpus, columns=["Text"])
         corpus = corpus_df["Text"].drop_duplicates().values.tolist()
     elif "msmarco" in DATASET:
-        corpus_all, _, _ = load_dataset(dataset="msmarco")
         count = int(DATASET.split('_')[1])
-        corpus_gen = (value for i, value in enumerate(corpus_all.values()) if i < count)
+        corpus, _, _ = load_dataset(dataset="msmarco", length=count)
         sep = " "
-        corpus = [(doc["title"] + sep + doc["text"]).strip() for doc in corpus_gen]
-        del corpus_gen
-        del corpus_all
+        corpus = [(doc["title"] + sep + doc["text"]).strip() for doc in corpus.values()]
     else:
         raise ValueError("Unknown dataset:", DATASET)
 
