1e267dd98fcc58e40957e8a534cc3157f6542552
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2025-03-10 22:04:13 +0400

    fix: move u to gpu

--- ivae/pl_model.py
+++ ivae/pl_model.py
@@ -30,6 +30,7 @@ class SparserModel(L.LightningModule):
 
         super().__init__()
         self.save_hyperparameters()
+        self.device = device
         backbone = AutoModel.from_pretrained(BACKBONE_MODEL_ID).to(device)
         backbone.eval()
         for p in backbone.parameters():
@@ -206,6 +207,6 @@ class SparserModel(L.LightningModule):
         x = self.backbone(input_ids=token_ids, attention_mask=token_mask)
         x = mean_pooling(model_output=x, attention_mask=token_mask) # TODO: try pool sparse embeddings
         _, labels = self.embs_kmeans.search(x.cpu().detach().numpy(), 1)
-        labels = torch.from_numpy(labels).squeeze()
+        labels = torch.from_numpy(labels).squeeze().to(self.device)
         u = torch.nn.functional.one_hot(labels, num_classes=self.aux_dim).float()
         return x, u
