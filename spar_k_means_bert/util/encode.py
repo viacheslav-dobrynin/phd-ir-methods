a5f651203db0d9b3b78ffb634086c9eb1641e5a0
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2025-05-24 15:39:31 +0400

    refactor: apply formatter

--- spar_k_means_bert/util/encode.py
+++ spar_k_means_bert/util/encode.py
@@ -5,6 +5,8 @@ import torch
 def encode_to_token_embs(model, input_ids, attention_mask):
     # Compute token embeddings
     with torch.no_grad():
-        model_output = model(input_ids=input_ids, attention_mask=attention_mask, return_dict=True)
+        model_output = model(
+            input_ids=input_ids, attention_mask=attention_mask, return_dict=True
+        )
 
     return model_output.last_hidden_state
