9e63fdf308b677abb39c578cd732776591bac9dd
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2025-08-17 17:38:16 +0400

    refactor: move args to the separate file

--- spar_k_means_bert/run.py
+++ spar_k_means_bert/run.py
@@ -1,4 +1,3 @@
-import argparse
 import os
 import pickle
 from collections import defaultdict
@@ -13,6 +12,7 @@ from transformers import AutoTokenizer
 
 from common.encode_dense_fun_builder import build_encode_dense_fun
 from common.model import load_model
+from spar_k_means_bert.args import get_args
 from spar_k_means_bert.dataset import get_dataset
 from spar_k_means_bert.in_memory_inverted_index import InMemoryInvertedIndex
 from spar_k_means_bert.lucene_index import LuceneIndex
@@ -134,25 +134,7 @@ def query_tokens_calculator(query):
 
 
 if __name__ == '__main__':
-    # Hyperparameters
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-mid', '--backbone-model-id', type=str, default='sentence-transformers/all-MiniLM-L6-v2', help='backbone model id (default sentence-transformers/all-MiniLM-L6-v2)')
-    parser.add_argument('-d', '--dataset', type=str, default='scifact', help='BEIR dataset name (default scifact)')
-    parser.add_argument('-l', '--dataset-length', type=int, default=None, help='Dataset length (default None, all dataset)')
-    parser.add_argument('-b', '--batch-size', type=int, default=128, help='batch size (default 128)')
-    parser.add_argument('-kmn', '--kmeans-n-clusters', type=int, default=8, help='kmeans clusters number (default 8)')
-    parser.add_argument('-M', '--hnsw-M', type=int, default=32, help='the number of neighbors used in the graph. A larger M is more accurate but uses more memory (default 32)')
-    parser.add_argument('-efs', '--hnsw-ef-search', type=int, default=16, help='HNSW ef search param (default 16)')
-    parser.add_argument('-efc', '--hnsw-ef-construction', type=int, default=40, help='HNSW ef construction param (default 40)')
-    parser.add_argument('-in', '--index-n-neighbors', type=int, default=8, help='index neighbors number (default 8)')
-    parser.add_argument('-stk', '--search-top-k', type=int, default=1000, help='search tok k results (default 1000)')
-    parser.add_argument('-sn', '--search-n-neighbors', type=int, default=3, help='search neighbors number (default 3)')
-    parser.add_argument('--train-hnsw-only', action="store_true", help='train hnsw only (default False)')
-    parser.add_argument('-imi', '--in-memory-index', action="store_true", help='in-memory inverted index type (default False)')
-    parser.add_argument('-c', '--use-cache', action="store_true", help='use cache (default False)')
-    parser.add_argument('-p', '--base-path', type=str, default='./', help='base path (default ./)')
-    args = parser.parse_args()
-    print(f"Params: {args}")
+    args = get_args()
     # Data, tokenizer, model
     tokenizer = AutoTokenizer.from_pretrained(args.backbone_model_id, use_fast=True)
     dataset, queries, qrels = get_dataset(tokenize=tokenize, dataset=args.dataset, length=args.dataset_length)
