aaad6e163d322e39b52b5d1208357760778b561a
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2025-08-17 18:27:43 +0400

    refactor: use lazy map for doc_id_to_embs

---
+++ spar_k_means_bert/util/map.py
@@ -0,0 +1,21 @@
+class LazyMap:
+    def __init__(self, builder) -> None:
+        self._builder = builder
+        self._result = None
+
+    def _get(self):
+        if not self._result:
+            self._result = self._builder()
+        return self._result
+
+    def __getitem__(self, k):
+        return self._get()[k]
+
+    def __iter__(self):
+        return iter(self._get())
+
+    def __len__(self):
+        return len(self._get())
+
+    def __getattr__(self, name):
+        return getattr(self._get(), name)
