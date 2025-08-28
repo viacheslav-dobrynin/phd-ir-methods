7e4c45e1e38a75b46648903753eb86108c37c14a
Viacheslav Dobrynin <w.v.dobr@gmail.com>
2025-08-28 22:13:25 +0400

    feat: update base path

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
+BASE_PATH=/opt/dlami/nvme
+
+echo "Changing location"
+cd "$BASE_PATH"
+echo "Changed location"
+
 echo "Installing gh repo"
 sudo apt install gh
 gh auth login
 git clone https://github.com/viacheslav-dobrynin/phd-ir-methods
+cd phd-ir-methods
+git checkout method1_filter_stopwords
 echo "Installed gh repo"
 
 echo "Installing conda"
@@ -15,18 +23,17 @@ wget -qO "$INST" "$URL"
 bash "$INST" -b -p "$CONDA_DIR"
 rm -f "$INST"
 "$CONDA_DIR/bin/conda" init bash >/dev/null
-source ~/.bashrc
 eval "$("$CONDA_DIR/bin/conda" shell.bash hook)"
 conda --version
 echo "Conda installed"
 
 echo "Installing conda env"
-conda env create -f ~/phd-ir-methods/spar_k_means_bert/conda_env.yml
+conda env create -f "$BASE_PATH/phd-ir-methods/spar_k_means_bert/conda_env.yml"
 conda activate spar-k-means-bert
 echo "Conda env installed and activated"
 
 echo "Preparing PyLucene"
-cd ~/phd-ir-methods/tools
+cd "$BASE_PATH/phd-ir-methods/tools"
 tar -xzf pylucene.tar.gz
 tar -xzf jcc.tar.gz
 cp jcc/libjcc3.so "$CONDA_PREFIX/lib"
@@ -35,12 +42,12 @@ export LD_LIBRARY_PATH="$CONDA_PREFIX/lib:/usr/lib/jvm/java-21-amazon-corretto/l
 echo "Prepared PyLucene"
 
 echo "Downloading Vector Distionary"
-aws s3 cp s3://spark-bert-vector-dictionary/uploads/hnsw.index ~/phd-ir-methods/hnsw.index
-aws s3 cp s3://spark-bert-vector-dictionary/uploads/faiss_idx_to_token.pickle ~/phd-ir-methods/faiss_idx_to_token.pickle
+aws s3 cp s3://spark-bert-vector-dictionary/uploads/hnsw.index "$BASE_PATH/phd-ir-methods/hnsw.index"
+aws s3 cp s3://spark-bert-vector-dictionary/uploads/faiss_idx_to_token.pickle "$BASE_PATH/phd-ir-methods/faiss_idx_to_token.pickle"
 echo "Downloaded Vector Distionary"
 
 echo "Running SparkBERT"
-cd ~/phd-ir-methods
+cd "$BASE_PATH/phd-ir-methods"
 LD_LIBRARY_PATH="$CONDA_PREFIX/lib:/usr/lib/jvm/java-21-amazon-corretto/lib/server/"
-python -m spar_k_means_bert.run --use-cache
+python -m spar_k_means_bert.run --use-cache -d msmarco
 echo "Finished"
